d4e001c9c5b36d1b9b3ad958795d24fbaa3f8a95
msk102948 <msk102948@gmail.com>
2023-12-11 05:49:30 +0900

    coding interview MostCommonWords

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import algorithm_interview.MostCommonWords;
 import algorithm_interview.ReorderLogFiles;
 import algorithm_interview.ReverseString;
 import algorithm_interview.ValidPalindrome;
 
 import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -15,8 +17,12 @@ public class Main {
 //        char[] s = {'r', 'a', 'c', 'e', 'c', 'a', 'r'};
 //        reverseString.reversStringCharArray(s);
 
-        String[] logs = {"id1 8 1 5 1", "id2 art can", "id3 3 6", "id4 own kit dig", "id5 art zero"};
-        System.out.println("reorderLogFiles : " + Arrays.toString(ReorderLogFiles.reorderLogFiles(logs)));
+//        String[] logs = {"id1 8 1 5 1", "id2 art can", "id3 3 6", "id4 own kit dig", "id5 art zero"};
+//        System.out.println("reorderLogFiles : " + Arrays.toString(ReorderLogFiles.reorderLogFiles(logs)));
+
+        String paragraph = "Ross hit a ball, the hit BALL flew far away after it was hit.";
+        String[] banned = {"hit"};
+        System.out.println("mostCommonWords : "+ MostCommonWords.mostCommonWords(paragraph, banned));
     }
 }
 
